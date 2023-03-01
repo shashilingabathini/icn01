5b116f60ab200d898d7a56ee930afe5b205d9b4a
shashilingabathini <shashi.lingabathini@gmail.com>
2023-03-01 22:00:54 +0530

    fix 03

--- src/icm/api/plugin/WebContent/icn/adaptors/AddCaseAdaptor.js
+++ src/icm/api/plugin/WebContent/icn/adaptors/AddCaseAdaptor.js
@@ -1,17 +1,35 @@
-define(["dojo/_base/declare","icm/model/properties/controller/ControllerManager"],function(declare,ControllerManager) {
+define(["dojo/_base/declare","icm/model/properties/controller/ControllerManager","dojo/_base/lang"],function(declare,ControllerManager,lang) {
 
     return declare("icn/adaptors/AddCaseAdaptor", [] , {
+
+        enableWindowDebug : window.location.href.indexOf("enableWindowDebug") > 0 ,
+
+        enableConsoleDebugs : window.location.href.indexOf("enableConsoleDebugs") > 0,
 
         showCommentsByPOCName : function(adaptor , payload) {
             // adaptor is a script adaptor
             console.log('showCommentsByPOCName is called');
-            window.adaptor = adaptor;
-            window.payload = payload;
+            if(enableWindowDebug) {
+                window.adaptor = adaptor;
+                window.payload = payload;
+            }
             // on change of property type want to show other props in UI. we do not want to use icm.PropertyUpdated from Properties widget for this
             if(payload) {
                 var editable = payload.caseEditable;
                 var controller = this.bindController(editable);
-                window.controller = controller;
+                if(enableWindowDebug)
+                    window.controller = controller;
+                var prefix = payload.caseType.getSolution().getPrefix(); // solution prefix
+                if(enableConsoleDebugs)
+                    console.log(' solution prefix is '+prefix);
+                var pocTypeName =  prefix + "_" + "POCType";
+                var propertyController = controller.getPropertyController(pocTypeName);
+                propertyController.watch("value",lang.hitch(this,function(o  , n) {
+                    if(enableConsoleDebugs) {
+                        console.log(pocTypeName + " changes are detected in ui .");
+                        console.log("Old "+o+" , New "+n);
+                    }
+                })); // watch the changes of this field
             }
         },
         bindController : function(editable) {
