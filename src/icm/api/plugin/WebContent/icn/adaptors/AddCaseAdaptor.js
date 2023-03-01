d7c46c1e52e70c2ffe5f7e92fecb2ede0d35e713
shashilingabathini <shashi.lingabathini@gmail.com>
2023-03-01 20:11:24 +0530

    fix 02

--- src/icm/api/plugin/WebContent/icn/adaptors/AddCaseAdaptor.js
+++ src/icm/api/plugin/WebContent/icn/adaptors/AddCaseAdaptor.js
@@ -1,4 +1,4 @@
-define(["dojo/_base/declare"],function(declare) {
+define(["dojo/_base/declare","icm/model/properties/controller/ControllerManager"],function(declare,ControllerManager) {
 
     return declare("icn/adaptors/AddCaseAdaptor", [] , {
 
@@ -7,6 +7,18 @@ define(["dojo/_base/declare"],function(declare) {
             console.log('showCommentsByPOCName is called');
             window.adaptor = adaptor;
             window.payload = payload;
+            // on change of property type want to show other props in UI. we do not want to use icm.PropertyUpdated from Properties widget for this
+            if(payload) {
+                var editable = payload.caseEditable;
+                var controller = this.bindController(editable);
+                window.controller = controller;
+            }
+        },
+        bindController : function(editable) {
+            return ControllerManager.bind(editable);
+        },
+        unbindController : function(editable) {
+            return ControllerManager.unbind(editable);
         }
 
     });
